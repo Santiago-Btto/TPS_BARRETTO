cd0bac5d53ecbc15f0a3051596c130689fcead0e
Santiago <santiagobtto30@gmail.com>
2025-09-10 21:56:42 -0300

    Mejore el bingo con sample para el numero sacado

--- RIGONI/TP_08_9/bingo.py
+++ RIGONI/TP_08_9/bingo.py
@@ -3,12 +3,15 @@ print(bienvenida)
 
 import random
 
-number = random.sample(range(1, 51), 25)
+number = random.sample(range(1, 51), 25) # carton (25 números únicos del 1 al 50)
 bingo_card = [number[i:i+5] for i in range(0, 25, 5)]
 
-while True:
-    number_draw = random.randint(1, 51) #numero sorteado
+draw_numbers = random.sample(range(1,51), 50) #numero sorteado (50 numeros únicos del 1 al 50)
+
+for number_draw in draw_numbers:  # vamos sacando uno por uno
     print(f'Salió el número: {number_draw}')
+
+    found = False # para saber si el número está en el cartón
 
     for i in range(5):
         for j in range(5):
@@ -25,8 +28,15 @@ while True:
         print("¡Bingo!")
         break
 
+    if found:
+        print("El número está en el cartón.")
+        print('--------------------------------') # para 'separar'
+        continue
+
     else:
         print("El número no está en el cartón.")
         print('--------------------------------') # para 'separar'
         continue
+
+
     
